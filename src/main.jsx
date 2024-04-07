6026bfb559f7b25b2d82a43135119d621876ccaa
Oleksandr <115729512+oleksandrmaliy@users.noreply.github.com>
2024-04-07 22:22:33 +0300

    2

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from '../src/components/App';
+import { BrowserRouter } from 'react-router-dom';
 import './index.css';
 import { Provider } from 'react-redux';
 import { store } from './redux/store';
@@ -8,7 +9,9 @@ import { store } from './redux/store';
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <Provider store={store}>
-      <App />
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
     </Provider>
   </React.StrictMode>
 );
