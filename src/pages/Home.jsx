fc26415d88f0f17fa3b518abdb26faddb36f2e0e
Oleksandr <115729512+oleksandrmaliy@users.noreply.github.com>
2024-04-11 12:52:36 +0300

    ok4

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,33 +1,47 @@
+/* eslint-disable react/no-unescaped-entities */
 import PageTitle from '../components/PageTitle/PageTitle';
 
 export default function Home() {
   return (
     <div>
       <PageTitle>
-        Contact Book welcome page
+        Phone Book
         {/* <span role="img" aria-label="Greeting icon">
           💁‍♀️
         </span> */}
       </PageTitle>
       <p>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Magni
-        asperiores eius nobis saepe quis voluptatibus facere expedita tempora,
-        hic similique natus minima fugit doloribus molestiae dolorum sed debitis
-        tempore consectetur repellat, accusantium aspernatur incidunt
-        doloremque. Quod doloribus beatae, molestiae voluptatibus, numquam
-        recusandae accusamus voluptatem aliquam, sed vero consectetur esse
-        harum?
+        A telephone directory, commonly called a telephone book, telephone
+        address book, phonebook, or the white and yellow pages, is a listing of
+        telephone subscribers in a geographical area or subscribers to services
+        provided by the organization that publishes the directory. Its purpose
+        is to allow the telephone number of a subscriber identified by name and
+        address to be found. The advent of the Internet, search engines, and
+        smartphones in the 21st century greatly reduced the need for a paper
+        phone book. Some communities, such as Seattle and San Francisco, sought
+        to ban their unsolicited distribution as wasteful, unwanted and harmful
+        to the environment. The slogan "Let Your Fingers Do the Walking" refers
+        to use of phone books.
       </p>
       <p>
-        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Dolorum et
-        illo non vel rerum odit, maxime, vitae dolorem molestiae error
-        cupiditate! Quasi eaque est provident voluptates adipisci? Cumque
-        quisquam laboriosam ullam, inventore ipsa ducimus quaerat dolorum natus
-        quasi amet ab earum vero, laborum cum quibusdam qui? Illo ab facere a
-        dolore, labore quisquam modi voluptatum esse nisi accusantium architecto
-        qui veritatis molestiae doloribus quas placeat, suscipit asperiores
-        fugiat quam sint perspiciatis ducimus! Officiis quam vel inventore fuga
-        dolorum, voluptatibus possimus.
+        Subscriber names are generally listed in alphabetical order, together
+        with their postal or street address and telephone number. In principle
+        every subscriber in the geographical coverage area is listed, but
+        subscribers may request the exclusion of their number from the
+        directory, often for a fee; their number is then said to be "unlisted"
+        (US and Canada), "ex-directory" (British English), or "private"
+        (Australia and New Zealand). A telephone directory may also provide
+        instructions: how to use the telephone service, how to dial a particular
+        number, be it local or international, what numbers to access important
+        and emergency services, utilities, hospitals, doctors, and organizations
+        who can provide support in times of crisis. It may also have civil
+        defense, emergency management, or first aid information. There may be
+        transit maps, postal code/zip code guides, international dialing codes
+        or stadium seating charts, as well as advertising. In the US, under
+        current rules and practices, mobile phone and voice over IP listings are
+        not included in telephone directories. Efforts to create cellular
+        directories have met stiff opposition from several fronts, including
+        those who seek to avoid telemarketers.
       </p>
     </div>
   );
--- src/redux/auth/slice.js
+++ src/redux/auth/slice.js
@@ -16,7 +16,7 @@
 // refreshUser - оновлення користувача за токеном. Базовий тип екшену "auth/refresh". Використовується у компоненті App під час його монтування.
 
 import { createSlice } from '@reduxjs/toolkit';
-import { logIn, logOut, registration } from './operations';
+// import { logIn, logOut, registration } from './operations';
 
 const authSlice = createSlice({
   name: 'auth',
@@ -29,26 +29,25 @@ const authSlice = createSlice({
     isLoggedIn: false,
     isRefreshing: false,
   },
-  extraReducers: builder =>
-    builder
-      .addCase(registration.fulfilled, (state, action) => {
-        state.user = action.payload.user;
-        state.token = action.payload.token;
-        state.isLoggedIn = true;
-      })
-      .addCase(logIn.fulfilled, (state, action) => {
-        state.user = action.payload.user;
-        state.token = action.payload.token;
-        state.isLoggedIn = true;
-      })
-      .addCase(logOut.fulfilled, state => {
-        state.user = {
-          name: null,
-          email: null,
-        };
-        state.token = null;
-        state.isLoggedIn = false;
-      }),
+  extraReducers: builder => builder,
+  //   .addCase(registration.fulfilled, (state, action) => {
+  //     state.user = action.payload.user;
+  //     state.token = action.payload.token;
+  //     state.isLoggedIn = true;
+  //   })
+  //   .addCase(logIn.fulfilled, (state, action) => {
+  //     state.user = action.payload.user;
+  //     state.token = action.payload.token;
+  //     state.isLoggedIn = true;
+  //   })
+  //   .addCase(logOut.fulfilled, state => {
+  //     state.user = {
+  //       name: null,
+  //       email: null,
+  //     };
+  //     state.token = null;
+  //     state.isLoggedIn = false;
+  //   }),
 });
 
 export const authReducer = authSlice.reducer;
