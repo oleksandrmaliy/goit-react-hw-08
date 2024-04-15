421ee1dedb88f3b7c892cca7765e247abf95ce06
Oleksandr <115729512+oleksandrmaliy@users.noreply.github.com>
2024-04-15 10:56:06 +0300

    intermediate

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -14,8 +14,6 @@ const RegistrationPage = lazy(() => import('../pages/Registration'));
 const LoginPage = lazy(() => import('../pages/Login'));
 const ContactsPage = lazy(() => import('../pages/Contacts'));
 
-// import Contacts from '../pages/Contacts';
-
 import './App.css';
 
 const App = () => {
@@ -55,29 +53,3 @@ const App = () => {
 };
 
 export default App;
-
-// import { lazy, Suspense } from "react";
-// import { Route, Routes } from "react-router-dom";
-// import { Toaster } from "react-hot-toast";
-// import Layout from "./Layout/Layout";
-
-// const HomePage = lazy(() => import("../pages/Home"));
-// const RegisterPage = lazy(() => import("../pages/Register"));
-// const LoginPage = lazy(() => import("../pages/Login"));
-// const TasksPage = lazy(() => import("../pages/Tasks"));
-
-// export default function App() {
-//   return (
-//     <Layout>
-//       <Suspense fallback={null}>
-//         <Routes>
-//           <Route path="/" element={<HomePage />} />
-//           <Route path="/register" element={<RegisterPage />} />
-//           <Route path="/login" element={<LoginPage />} />
-//           <Route path="/tasks" element={<TasksPage />} />
-//         </Routes>
-//       </Suspense>
-//       <Toaster />
-//     </Layout>
-//   );
-// }
